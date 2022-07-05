fd822f07a6877edfb8a82db9786a388345d5b60a
Intext <vasif_zv@bk.ru>
2022-07-05 12:46:33 +0400

    ikinci is goruldu ve yoxla.

--- src/Telebe.java
+++ src/Telebe.java
@@ -1,5 +1,9 @@
 public class Telebe {
     public static void main(String[] args) {
-        System.out.println("Salam Dunya");
+
+
+
+        System.out.println("Salam Dunya 2");
+        System.out.println("Salam Dunya 3");
     }
 }
